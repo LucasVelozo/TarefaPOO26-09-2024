48cd080021fd3962ddc4bf45482ab20e285d8ea9
Lucas Velozo da Silva <90939313+LucasVelozo@users.noreply.github.com>
2024-09-30 23:58:41 -0300

    Add files via upload

---
+++ POO2Ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace POO2Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Maior de 2 números==");
+
+            Maior m;
+            m = new Maior();
+
+            Console.Write("Digite o primeiro número: ");
+            m.setn1(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite o segundo número: ");
+            m.setn2(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Maior: {0} ({1} número)", m.indicarMaior(), m.getns());
+        }
+    }
+}
